93088ffe6705f49f6f3c999aea00aebe3f8896cb
aritify <thongh@oregonstate.edu>
2025-04-14 17:06:03 +0000

    added trial to loop

--- loop.bash
+++ loop.bash
@@ -4,7 +4,7 @@
 for t in 1 2 4 8
 do
     # number of trials
-    for n in 100 1000 10000 50000 100000 500000
+    for n in 100 1000 10000 50000 100000 500000 1000000
     do
         g++ proj1.cpp -DNUMT=$t -DNUMTRIALS=$n -o proj1 -lm -fopenmp
         ./proj1
