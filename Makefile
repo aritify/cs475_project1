7468a1e0a4a0498a66c4410870d4b3a9cb60b36b
aritify <thongh@oregonstate.edu>
2025-04-14 16:24:07 +0000

    complete program

---
+++ Makefile
@@ -0,0 +1,2 @@
+proj1:	proj1.cpp
+		g++ proj1.cpp -o proj1 -lm -fopenmp
---
+++ loop.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# number of threads
+for t in 1 2 4 8
+do
+    # number of trials
+    for n in 100 1000 10000 50000 100000
+    do
+        g++ proj1.cpp -DNUMT=$t -DNUMTRIALS=$n -o proj1 -lm -fopenmp
+        ./proj1
+    done
+done
